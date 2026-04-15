f4173802420a78e4b058eff5454d631f03ec8b97
xiongtianchen <xiongtianchen1@huawei.com>
2026-04-15 16:26:20 +0800

    add discover_skill to BuiltinToolSource::discover

--- crates/tool/src/impl/builtin/tool_source.rs
+++ crates/tool/src/impl/builtin/tool_source.rs
@@ -10,6 +10,7 @@ use super::glob::discover_glob;
 use super::grep::discover_grep;
 use super::join_subagent::discover_join_subagent;
 use super::print_hello_world::discover_print_hello_world;
+use super::skill::discover_skill;
 use super::spawn_subagent::discover_spawn_subagent;
 use super::webfetch::discover_webfetch;
 use crate::r#impl::ToolRuntimeServices;
@@ -41,6 +42,7 @@ impl ToolSource for BuiltinToolSource {
             discover_webfetch(),
             discover_spawn_subagent(self.services.clone()),
             discover_join_subagent(self.services.clone()),
+            discover_skill(),
         ]
     }
 }
